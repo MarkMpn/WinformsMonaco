c807b094d64a6a320f3e28886cf2ce481dd4daf4
Mark Carrington <31017244+MarkMpn@users.noreply.github.com>
2025-11-09 17:03:30 +0000

    Copilot progress

--- TestApps/NetCore/Form1.Designer.cs
+++ TestApps/NetCore/Form1.Designer.cs
@@ -29,17 +29,29 @@
         private void InitializeComponent()
         {
             topPanel = new Panel();
+            authButton = new Button();
             showMinimapCheckBox = new CheckBox();
             addLspButton = new Button();
             setTextButton = new Button();
             getTextButton = new Button();
             setLangButton = new Button();
+            statusStrip1 = new StatusStrip();
+            statusLabel = new ToolStripStatusLabel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
             bodyPanel = new Panel();
+            tabPage2 = new TabPage();
+            lspLogTextBox = new TextBox();
             topPanel.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // topPanel
             // 
+            topPanel.Controls.Add(authButton);
             topPanel.Controls.Add(showMinimapCheckBox);
             topPanel.Controls.Add(addLspButton);
             topPanel.Controls.Add(setTextButton);
@@ -50,6 +62,16 @@
             topPanel.Name = "topPanel";
             topPanel.Size = new Size(800, 73);
             topPanel.TabIndex = 0;
+            // 
+            // authButton
+            // 
+            authButton.Location = new Point(336, 12);
+            authButton.Name = "authButton";
+            authButton.Size = new Size(75, 23);
+            authButton.TabIndex = 2;
+            authButton.Text = "Auth";
+            authButton.UseVisualStyleBackColor = true;
+            authButton.Click += authButton_Click;
             // 
             // showMinimapCheckBox
             // 
@@ -102,26 +124,94 @@
             setLangButton.UseVisualStyleBackColor = true;
             setLangButton.Click += setLangButton_Click;
             // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { statusLabel });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(66, 17);
+            statusLabel.Text = "statusLabel";
+            statusLabel.Visible = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 73);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 355);
+            tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(bodyPanel);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 327);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Monaco";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
             // bodyPanel
             // 
             bodyPanel.Dock = DockStyle.Fill;
-            bodyPanel.Location = new Point(0, 73);
+            bodyPanel.Location = new Point(3, 3);
             bodyPanel.Name = "bodyPanel";
-            bodyPanel.Size = new Size(800, 377);
-            bodyPanel.TabIndex = 0;
+            bodyPanel.Size = new Size(786, 321);
+            bodyPanel.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(lspLogTextBox);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 327);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "LSP Log";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lspLogTextBox
+            // 
+            lspLogTextBox.Dock = DockStyle.Fill;
+            lspLogTextBox.Location = new Point(3, 3);
+            lspLogTextBox.Multiline = true;
+            lspLogTextBox.Name = "lspLogTextBox";
+            lspLogTextBox.ReadOnly = true;
+            lspLogTextBox.ScrollBars = ScrollBars.Vertical;
+            lspLogTextBox.Size = new Size(786, 321);
+            lspLogTextBox.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(bodyPanel);
+            Controls.Add(tabControl1);
+            Controls.Add(statusStrip1);
             Controls.Add(topPanel);
             Name = "Form1";
             Text = "Form1";
             topPanel.ResumeLayout(false);
             topPanel.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -130,8 +220,15 @@
         private Button setTextButton;
         private Button getTextButton;
         private Button setLangButton;
-        private Panel bodyPanel;
         private Button addLspButton;
         private CheckBox showMinimapCheckBox;
+        private StatusStrip statusStrip1;
+        private Button authButton;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Panel bodyPanel;
+        private TabPage tabPage2;
+        private TextBox lspLogTextBox;
+        private ToolStripStatusLabel statusLabel;
     }
 }
--- TestApps/NetCore/Form1.cs
+++ TestApps/NetCore/Form1.cs
@@ -2,12 +2,17 @@ using LSP;
 using Nerdbank.Streams;
 using MarkMpn.WinformsMonaco;
 using StreamJsonRpc;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace NetCore
 {
     public partial class Form1 : Form
     {
         private readonly Monaco _monaco;
+        private JsonRpc _rpcClient;
 
         public Form1()
         {
@@ -41,15 +46,101 @@ namespace NetCore
 
         private void addLspButton_Click(object sender, EventArgs e)
         {
-            var (clientStream, serverStream) = FullDuplexStream.CreatePair();
+            var psi = new ProcessStartInfo(@"C:\Users\mark_\Downloads\copilot-language-server.exe");
+            psi.ArgumentList.Add("--stdio");
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            var process = Process.Start(psi);
+            /*var (clientStream, serverStream) = FullDuplexStream.CreatePair();
             var rpcServer = new LspServer(serverStream);
-            var rpcClient = new JsonRpc(clientStream);
-            _monaco.RegisterLSPClient(_monaco.Language, rpcClient);
+            var rpcClient = new JsonRpc(clientStream);*/
+            _rpcClient = new JsonRpc(process.StandardInput.BaseStream, process.StandardOutput.BaseStream);
+            _rpcClient.TraceSource.Switch.Level = SourceLevels.All;
+            _rpcClient.TraceSource.Listeners.Add(new ConsoleTraceListener());
+
+            _rpcClient.AddLocalRpcTarget(new LspHandlers(this));
+
+            _monaco.RegisterLSPClient(_monaco.Language, _rpcClient);
         }
 
         private void showMinimapCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             _monaco.MinimapVisible = showMinimapCheckBox.Checked;
+        }
+
+        private void InvokeIfRequired(Action action)
+        {
+            if (InvokeRequired)
+                Invoke(action);
+            else
+                action();
+        }
+
+        private async void authButton_Click(object sender, EventArgs e)
+        {
+            var response = await _rpcClient.InvokeAsync<AuthResponse>("signIn", new { });
+        }
+
+        class AuthResponse
+        {
+            [JsonPropertyName("userCode")]
+            public string UserCode { get; set; }
+            [JsonPropertyName("command")]
+            public AuthCommand Command { get; set; }
+        }
+
+        class AuthCommand
+        {
+            [JsonPropertyName("command")]
+            public string Command { get; set; }
+            [JsonPropertyName("arguments")]
+            public string[] Arguments { get; set; }
+            [JsonPropertyName("title")]
+            public string Title { get; set; }
+        }
+
+        public class LspHandlers
+        {
+            private readonly Form1 _form;
+
+            public LspHandlers(Form1 form)
+            {
+                _form = form;
+            }
+
+            [JsonRpcMethod("window/logMessage", UseSingleObjectParameterDeserialization = true)]
+            public void HandleLogMessage(LogMessageParams param)
+            {
+                _form.lspLogTextBox.Text += $"[{param.Type}] {param.Message}{Environment.NewLine}";
+            }
+
+            [JsonRpcMethod("didChangeStatus", UseSingleObjectParameterDeserialization = true)]
+            public void ChangeStatus(ChangeStatusParams param)
+            {
+                _form.statusLabel.Text = $"[{param.Kind}] {param.Message}";
+                _form.statusLabel.Visible = true;
+            }
+
+            public class LogMessageParams
+            {
+                public MessageType Type { get; set; }
+                public string Message { get; set; }
+            }
+
+            public enum MessageType
+            {
+                Error = 1,
+                Warning = 2,
+                Info = 3,
+                Log = 4,
+                Debug = 5,
+            }
+
+            public class ChangeStatusParams
+            {
+                public string Kind { get; set; }
+                public string Message { get; set; }
+            }
         }
     }
 }
